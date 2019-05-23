4e6477a3c18c654386d4b876924330f934cb60ef
李子淇 <lzzzzzq@liziqideMacBook-Pro.local>
2019-05-23 15:01:44 +0800

    init

---
+++ index.js
@@ -0,0 +1,51 @@
+module.exports = {
+    // Extend website resources and html
+    website: {
+        assets: "./book",
+        js: [
+            "index.js"
+        ],
+        css: [
+            "index.css"
+        ],
+        html: {
+            "html:start": function() {
+                return "<!-- Start book "+this.options.title+" -->"
+            },
+            "html:end": function() {
+                return "<!-- End of book "+this.options.title+" -->"
+            },
+
+            "head:start": "<!-- head:start -->",
+            "head:end": "<!-- head:end -->",
+
+            "body:start": "<!-- body:start -->",
+            "body:end": "<!-- body:end -->"
+        }
+    },
+
+    // Extend ebook resources and html
+    website: {
+        assets: "./book",
+        js: [
+            "index.js"
+        ],
+        css: [
+            "index.css"
+        ],
+        html: {
+            "html:start": function() {
+                return "<!-- Start book "+this.options.title+" -->"
+            },
+            "html:end": function() {
+                return "<!-- End of book "+this.options.title+" -->"
+            },
+
+            "head:start": "<!-- head:start -->",
+            "head:end": "<!-- head:end -->",
+
+            "body:start": "<!-- body:start -->",
+            "body:end": "<!-- body:end -->"
+        }
+    }
+};
